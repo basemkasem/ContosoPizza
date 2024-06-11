9d4dea924001bd0a43ef4ac1a6bd99529f1a8625
basemkasem <basem.kasem16@gmail.com>
2024-06-12 01:02:29 +0300

    Add Pizza model

---
+++ ContosoPizza/Models/Pizza.cs
@@ -0,0 +1,9 @@
+﻿namespace ContosoPizza.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsGlutenFree { get; set; }
+    }
+}
