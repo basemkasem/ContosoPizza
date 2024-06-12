16d93a3941aee6b02d1990522b78a29630024d6a
basemkasem <basem.kasem16@gmail.com>
2024-06-12 08:42:13 +0300

    - Create Pizza Controller. - Implement GET verps.

---
+++ ContosoPizza/Controllers/PizzaController.cs
@@ -0,0 +1,33 @@
+﻿using ContosoPizza.Models;
+using ContosoPizza.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoPizza.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PizzaController : ControllerBase
+{
+    public PizzaController()
+    {
+
+    }
+
+    [HttpGet]
+    public ActionResult<List<Pizza>> GetAll() =>
+        PizzaService.GetAll();
+
+    [HttpGet("{id}")]
+    public ActionResult<Pizza> Get(int id)
+    {
+        var pizza = PizzaService.Get(id);
+        
+        if (pizza == null)
+        {
+            return NotFound();
+        }
+
+        return pizza;
+    }
+}
